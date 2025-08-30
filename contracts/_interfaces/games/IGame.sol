c172178b384ff654be8ec2ca6a4b2a8f4d553135
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2025-08-30 21:14:30 +0400

    IGame interface

---
+++ contracts/_interfaces/games/IGame.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+interface IGame {
+    function minBetAmount() external view returns (uint256 registered);
+
+    function maxBetAmount() external view returns (uint256 registered);
+
+    function houseEdge() external view returns (uint8 registered);
+
+    function isPaused() external view returns (bool registered);
+}
