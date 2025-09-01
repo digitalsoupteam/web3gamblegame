521152e8acd40e272c579fa8e0b251b7af28d6c1
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2025-09-01 16:01:12 +0400

    GameManager ignition

---
+++ ignition/modules/games/GameManager.ts
@@ -0,0 +1,12 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+import AddressBookModule from '../access/AddressBook';
+
+export default buildModule('GameManager', m => {
+  const { addressBookProxy } = m.useModule(AddressBookModule);
+
+  const impl = m.contract('GameManager');
+  const initData = m.encodeFunctionCall(impl, 'initialize', [addressBookProxy]);
+  const proxy = m.contract('ERC1967Proxy', [impl, initData]);
+
+  return { accessRolesProxy: proxy, accessRolesImpl: impl };
+});
