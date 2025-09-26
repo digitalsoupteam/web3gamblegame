90840d20ddfb88731de2b6b55166a59f412358a8
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2025-09-26 15:47:20 +0400

    Update GameManager ignition

--- ignition/modules/games/GameManager.ts
+++ ignition/modules/games/GameManager.ts
@@ -8,5 +8,7 @@ export default buildModule('GameManager', m => {
   const initData = m.encodeFunctionCall(impl, 'initialize', [addressBookProxy]);
   const proxy = m.contract('ERC1967Proxy', [impl, initData]);
 
+  m.call(addressBookProxy, 'initialSetGameManager', [proxy]);
+
   return { accessRolesProxy: proxy, accessRolesImpl: impl };
 });
